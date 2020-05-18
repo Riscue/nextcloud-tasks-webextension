53f5e2834d83c64a3ae1e992b25e52b215a00fff
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-05-19 01:31:52 +0300

    TODO items fetched

--- src/js/services/SelectorService.js
+++ src/js/services/SelectorService.js
@@ -1,8 +1,9 @@
 export const SelectorService = {
     discover:                   'd\\:current-user-principal d\\:href',
     calendarHomeSet:            'cal\\:calendar-home-set d\\:href',
-    calendarData:               'cal\\:supported-calendar-component-set cal\\:comp [name="VTODO"]',
+    calendarData:               'cal\\:supported-calendar-component-set cal\\:comp[name="VTODO"]',
     calendarDataHref:           'd\\:href',
     calendarDataDisplayName:    'd\\:displayname',
     calendarDataGetCtag:        'cs\\:getctag',
+    downloadCalendar:           ''
 };

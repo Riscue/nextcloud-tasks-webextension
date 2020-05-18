c91c5e731943e87bd6ca182333c94c72ef722d33
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-05-19 00:37:17 +0300

    Promise context switch on call

--- src/js/services/ApiService.js
+++ src/js/services/ApiService.js
@@ -2,23 +2,35 @@ import axios from 'axios'
 
 export const ApiService = {
 
-    _401interceptor: null,
-
     init(baseURL) {
         axios.defaults.baseURL = baseURL;
     },
 
-    propfind(url, data, credentials, config) {
-        config = config || {};
+    propfind(url, data, credentials, config = {}) {
         config.method = "PROPFIND";
         config.url = url;
         config.data = data;
-        config.auth = credentials || undefined;
-
+        config.auth = credentials;
         return axios.request(config);
     },
 
-    customRequest(data) {
-        return axios.request(data);
-    }
+    get(url, credentials, config = {}) {
+        config.auth = credentials;
+        return axios.get(url, config);
+    },
+
+    post(url, data, credentials, config = {}) {
+        config.auth = credentials;
+        return axios.post(url, data, config);
+    },
+
+    put(url, data, credentials, config = {}) {
+        config.auth = credentials;
+        return axios.put(url, data, config);
+    },
+
+    delete(url, credentials, config = {}) {
+        config.auth = credentials;
+        return axios.delete(url, config);
+    },
 };
--- src/js/services/UserService.js
+++ src/js/services/UserService.js
@@ -5,44 +5,33 @@ import {AuthenticationError} from "@js/errors/AuthenticationError";
 export const UserService = {
 
     async login(username, password) {
-        const requestData = {
-            method: 'get',
-            url: "/ocs/v2.php/core/getapppassword",
-            headers: {"OCS-APIRequest": true},
-            auth: {
-                username: username,
-                password: password
-            }
-        };
-
         try {
-            const response = await ApiService.customRequest(requestData);
-            const apppassword = response.data.ocs.data.apppassword;
+            const response = await ApiService.get(
+                '/ocs/v2.php/core/getapppassword',
+                {username: username, password: password},
+                {headers: {"OCS-APIRequest": true}}
+            );
 
+            const apppassword = response.data.ocs.data.apppassword;
             StorageService.save(StorageService.USERNAME, username);
             StorageService.save(StorageService.APP_PASSWORD, apppassword);
-
-            return apppassword;
         } catch (error) {
-            throw new AuthenticationError(error.response.status, error.response.data.detail)
+            throw new AuthenticationError(error.response.status, error.response.data.detail);
         }
     },
 
     async logout() {
         try {
-            const requestData = {
-                method: 'delete',
-                url: "/ocs/v2.php/core/apppassword",
-                headers: {"OCS-APIRequest": true},
-                auth: {
-                    username: StorageService.get(StorageService.USERNAME),
-                    password: StorageService.get(StorageService.APP_PASSWORD)
-                }
-            };
-            ApiService.customRequest(requestData);
+            await ApiService.delete(
+                '/ocs/v2.php/core/apppassword',
+                UserService.getCredentials(),
+                {headers: {"OCS-APIRequest": true}}
+            );
+
+            StorageService.clear();
         } catch (error) {
+            throw new AuthenticationError(error.response.status, error.response.data.detail);
         }
-        StorageService.clear();
     },
 
     isLoggedIn() {

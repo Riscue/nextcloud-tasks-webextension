e90cf11fa66ba74a65bfabeb0da6f632aa4b8d9c
İbrahim Akyel <ibrahim@ibrahimakyel.com>
2020-05-20 12:19:20 +0300

    Theme is applied

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -1,18 +1,1 @@
-import {BrowserApi} from '@tsP/browser-api';
-
-function processMessage(msg, sender) {
-    console.log(msg.type);
-}
-
-function onEvent(str) {
-    console.log(str);
-}
-
-const browser = BrowserApi.getBrowserApi();
-browser.runtime.onMessage.addListener(processMessage);
-
-browser.tabs.onActivated.addListener(() => onEvent('tabs.onActivated'));
-browser.tabs.onCreated.addListener(() => onEvent('tabs.onCreated'));
-browser.tabs.onUpdated.addListener(() => onEvent('tabs.onUpdated'));
-browser.tabs.onReplaced.addListener(() => onEvent('tabs.onReplaced'));
-browser.storage.onChanged.addListener(() => onEvent('storage.onChanged'));
+console.log('background.ts');
--- src/ts/options.ts
+++ src/ts/options.ts
@@ -1,11 +1,6 @@
 import Vue from 'vue';
-import {MdButton} from 'vue-material/dist/components';
-import OptionsComponent from '@vue/options/OptionsComponent.vue'
-import {ContextHelper} from '@ts/helpers/context.helper';
-import '@scss/main.scss';
-
-Vue.use(MdButton);
-ContextHelper.buildContext();
+import OptionsComponent from '@vue/options/OptionsComponent.vue';
+import './configure.ts'
 
 new Vue({
     el: '#vue-container',
